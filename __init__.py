0b4b93fc94ab199020145d1a7d1c522de4706769
Piotr Wreczycki <netf@netf.io>
2020-07-09 13:42:30 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from airflow.plugins_manager import AirflowPlugin
+from salesforce_plugin.hooks.salesforce_hook import SalesforceHook
+from salesforce_plugin.operators.salesforce_schema_to_redshift_operator import SalesforceSchemaToRedshiftOperator
+from salesforce_plugin.operators.salesforce_to_s3_operator import SalesforceBulkQueryToS3Operator
+from salesforce_plugin.operators.salesforce_to_s3_operator import SalesforceToS3Operator
+
+
+class SalesforcePlugin(AirflowPlugin):
+    name = "salesforce_plugin"
+    hooks = [SalesforceHook]
+    operators = [SalesforceToS3Operator,
+                 SalesforceSchemaToRedshiftOperator,
+                 SalesforceBulkQueryToS3Operator]
+    executors = []
+    macros = []
+    admin_views = []
+    flask_blueprints = []
+    menu_links = []
